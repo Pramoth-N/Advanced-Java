51489273e8fdf106b3797f0f0170d2630553f9cd
Pramothsgp <jarvis.tech.006@mail.com>
2024-07-23 00:29:13 +0530

    ctreated ASCIIConverter.java

--- day1/workouts/ASCIIConverter.java
+++ day1/workouts/ASCIIConverter.java
@@ -1,3 +1,16 @@
+/*
+ Given a single character input, Find and display its ASCII value. 
+ If the input is not a valid single character, display an appropriate error message.
+Input Format
+• The input consists of a single character.
+Output Format
+• If the input is a valid single character, output the ASCII value of the character.
+• If the input is not a valid single character, print"Invalid input".
+Constraints
+• The input will be a single character.
+• The character must be a printable ASCII (32 <= ASCII value <= 126).
+ */
+
 import java.util.Scanner;
 
 public class ASCIIConverter {
